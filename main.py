45c2babbca429f54457253b67a95e706eeb2c60b
Kucukcollu <kucukcollu@outlook.com>
2021-08-24 15:31:04 +0300

    Videos folder bug fixed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, Response, request
 import video_detection
-import time
 from pytube import YouTube
 import os
 
@@ -13,18 +12,18 @@ def index():
     return render_template("index.html")
 
 
-# original video should be here
+# get user input and install related video
 @app.route("/video", methods=["GET", "POST"])
 def video():
     global video
     if request.method == "POST":
         video_url = request.form.get("user_url")
         video = YouTube(video_url)
+        os.remove("/home/robot/projects/yolo_video_detection/videos/test.mp4")
         os.chdir("../yolo_video_detection/videos")
         video.streams.filter(
             progressive=True, file_extension='mp4', res="360p").first().download()
         os.chdir("/home/robot/projects/yolo_video_detection/videos/")
-
         for file in os.listdir("/home/robot/projects/yolo_video_detection/videos/"):
             old_name = file
             new_name = "test.mp4"
@@ -34,7 +33,7 @@ def video():
         return render_template("video.html")
 
 
-# detected video should be here
+# detected video response function
 @app.route("/detected")
 def detected():
     detect = video_detection.Detection("test.mp4")
